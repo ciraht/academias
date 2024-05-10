932baf533f1d70296c01871ff1ef8fe90dea025b
ciraht <tharictalon@gmail.com>
2024-05-10 08:57:28 -0300

    venv funcionando
    muito importante essa comit

---
+++ app.py
@@ -0,0 +1,16 @@
+# from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('teste.html', resultado='')
+
+
+# @app.route('/verificar', methods=['POST']) 
+# def verificar():
+
+#     colocar calculadora aqui
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
